1e293fc1bbce9ecbb557e4dc319ff071636140c5
P4blo <54816633+pabg92@users.noreply.github.com>
2024-04-14 22:20:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Terminal from './components/Terminal';
-import Navigation from './components/Navigation';
-import './css/App.css';
+import Terminal from './components/Terminal';  // Correct path to Terminal.js
+import Navigation from './components/Navigation';  // Correct path to Navigation.js
+import './css/App.css';  // Assuming App.css is under the css directory within the same folder
 
 function App() {
   return (
