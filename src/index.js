472210909fbf276fcdd64f0081b1e67d2155fb8c
P4blo <54816633+pabg92@users.noreply.github.com>
2024-04-14 22:20:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';  // Assuming App.js is in the same folder as index.js
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
